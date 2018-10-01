03f1a8ddeaa7c1293e989e3f2ac6f07cb56470df
v79 <liamjdavison@gmail.com>
2018-10-01 21:41:57 +0100

    A few predefined formats for LocalDateFormatter. Should I turn this into a LocalDateTime formatter too?

--- src/main/kotlin/org/liamjd/bascule/render/LocalDateFormatter.kt
+++ src/main/kotlin/org/liamjd/bascule/render/LocalDateFormatter.kt
@@ -5,16 +5,44 @@ import com.github.jknack.handlebars.Options
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+/**
+ * Only handles LocalDate with no time component
+ */
 class LocalDateFormatter(val inputFormat: String) : Helper<Any> {
+
+	private val ISO_DATE = "ISO_DATE"
+	private val ISO_LOCAL_DATE = "ISO_LOCAL_DATE"
+	private val ISO_ORDINAL_DATE = "ISO_ORDINAL_DATE"
+	private val ISO_WEEK_DATE = "ISO_WEEK_DATE"
 
 	override fun apply(value: Any?, options: Options): CharSequence {
 
 		if (value != null) {
-			val date = value as LocalDate
-			val outputFormatter: DateTimeFormatter
-			val pattern = options.param(0, options.hash<Any>("format", inputFormat)) as String
-			outputFormatter = DateTimeFormatter.ofPattern(pattern)
-			return outputFormatter.format(value)
+			val format = options.param(0, options.hash<Any>("format", inputFormat)) as String
+			val outputFormatter = when (format) {
+				ISO_DATE -> {
+					DateTimeFormatter.ISO_DATE
+				}
+				ISO_LOCAL_DATE -> {
+					DateTimeFormatter.ISO_LOCAL_DATE
+				}
+				ISO_ORDINAL_DATE -> {
+					DateTimeFormatter.ISO_ORDINAL_DATE
+				}
+				ISO_WEEK_DATE -> {
+					DateTimeFormatter.ISO_WEEK_DATE
+				}
+				else -> {
+					try {
+						DateTimeFormatter.ofPattern(format)
+					}  catch (iae: IllegalArgumentException) {
+						println("Could not parse localDate format string; reverting to ISO_LOCAL_DATE")
+						DateTimeFormatter.ISO_LOCAL_DATE
+					}
+				}
+			}
+			val result =	outputFormatter.format(value as LocalDate)
+			return result
 		}
 		return LocalDate.now().toString()
 	}
