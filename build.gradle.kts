2c4a4fdea56345a492c24da1079b1975609e64ae
Liam <liamjdavison@gmail.com>
2025-06-21 07:39:27 +0100

    Add GitHub Actions workflow for publishing releases and configure Maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,6 +106,18 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            groupId = "org.liamjd"
+            artifactId = "bascule"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/v79/bascule-static")
+            credentials {
+                username = (project.findProperty("gpr.user") ?: System.getenv("GH_USERNAME")) as String?
+                password = (project.findProperty("gpr.key") ?: System.getenv("GH_PAT_REPO")) as String?
+            }
         }
     }
 }
